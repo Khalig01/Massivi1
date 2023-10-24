d3b9b7dbb0968c29285291e804453de0e0143ae9
Khal01 <aliyev_56@list.ru>
2023-10-24 22:53:22 +0300

    massivi 1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+      int  [] apples = new int [3];
+      apples [0]= 1;
+    apples [2]= 2;
+        apples [3]=3;
+
+
+
+
+
+
+
+
+
+
+    }
+}
